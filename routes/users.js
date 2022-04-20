59f94479a9cfdfbec553497abeabb791f2d477cb
Artem Pavlov <Zireaeell@yandex.ru>
2022-04-20 11:11:07 +0300

    Исправление валидации

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getUsers,
@@ -8,11 +9,16 @@ const {
   updateUserAvatar,
 } = require('../controllers/users');
 
-const { validateUserId, validateUpdateUserInfo, validationUpdateAvatar } = require('../middlewares/validate');
+const { validateUserId, validationUpdateAvatar } = require('../middlewares/validate');
 
 router.get('/', getUsers);
 router.get('/me', getAuthorizedUser);
-router.patch('/me', validateUpdateUserInfo, updateUserInfo);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(20),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateUserInfo);
 router.patch('/me/avatar', validationUpdateAvatar, updateUserAvatar);
 router.get('/:userId', validateUserId, getUserById);
 
