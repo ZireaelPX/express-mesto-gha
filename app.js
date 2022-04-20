381c3f21f053e29060a5d5a7f3c3beddd68a450e
Artem Pavlov <Zireaeell@yandex.ru>
2022-04-20 10:20:47 +0300

    Исправление валидации

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
 const { createUser, login } = require('./controllers/users');
-
 const { validateCreateUser, validateLogin } = require('./middlewares/validate');
 
 const auth = require('./middlewares/auth');
-const errors = require('./middlewares/errors');
+const createErrors = require('./middlewares/errors');
 const routes = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
@@ -20,7 +20,7 @@ app.post('/signup', validateCreateUser, createUser);
 app.use(auth);
 app.use(routes);
 app.use(errors());
-app.use(errors);
+app.use(createErrors);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
