4a67fb3aad3ee0c26682aa620e33f432072a2b74
Artem Pavlov <Zireaeell@yandex.ru>
2022-04-21 09:43:45 +0300

    Исправление после ревью

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+const NotFound = require('../errors/not-found');
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 
-router.use((req, res) => {
-  res.status(404).send({ message: 'Страница не найдена. Проверьте URL' });
+router.use((req, res, next) => {
+  next(new NotFound('Страница не найдена. Проверьте URL'));
 });
 
 module.exports = router;
